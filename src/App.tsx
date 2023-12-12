0370be8e240ace95f135ba193c38eeb780d6ff72
lucastheldl <lucastheldl@gmail.com>
2023-12-12 16:31:37 -0300

    chore:added colors and font

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react"
 import "./App.css"
 
+
+
 function App() {
+
+  async function getUser(){
+    await fetch(`https://api.github.com/users/lucastheldl`).then(res=>res.json()).then(data=>console.log(data))
+  }
+
+  useEffect(()=>{
+    getUser();
+  },[])
+
   return(<div><h1>Hello</h1></div>)
   
 }
