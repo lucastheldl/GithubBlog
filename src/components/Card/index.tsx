ee1fe3b6935ffb536da1004a6d3ad9e54ddcec67
lucastheldl <lucastheldl@gmail.com>
2023-12-18 19:25:03 -0300

    feat: fetch post issue data in dynamic page

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 import styles from "./card.module.css";
 
 interface CardProps {
-  issue: { id: number; title: string; body: string };
+  issue: { number: number; title: string; body: string };
 }
 export function Card({ issue }: CardProps) {
   return (
-    <Link to={"/GithubBlog/posts/2"} className={styles.container}>
+    <Link to={`/GithubBlog/posts/${issue.number}`} className={styles.container}>
       <div className={styles.card_header}>
         <strong className={styles.title}>{issue.title}</strong>
         <span className={styles.date}>Datetime</span>
--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,7 +1,31 @@
-interface PostRouteParams {
-  id: string;
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+interface Issue {
+  number: number;
+  title: string;
+  body: string;
 }
 
 export function Post() {
-  return <h1>Post</h1>;
+  const { id } = useParams();
+
+  const [issue, setIssue] = useState<Issue | null>(null);
+
+  async function getIssueData() {
+    await fetch(
+      `https://api.github.com/repos/lucastheldl/GithubBlog/issues/${id}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setIssue(data);
+        console.log(data);
+      });
+  }
+
+  useEffect(() => {
+    getIssueData();
+  }, []);
+
+  return <>{issue && <h1>{issue.title}</h1>}</>;
 }
